449abbdf7ef3dae688003b523f6915b4a2d13013
rexno7 <rexno7@gmail.com>
2014-02-12 18:37:29 -0500

    CRUD finished

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,48 @@
 class PostsController < ApplicationController
-    def new
-    end
+  def new
+    @post = Post.new
+  end
 	
-	def create
-		render text: params[:post].inspect
-	end
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+        redirect_to @post
+    else
+        render "new"
+    end
+  end
+    
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    
+    redirect_to posts_path
+  end
+    
+  def index
+    @posts = Post.all
+  end
+    
+  private
+    def post_params
+      params.required(:post).permit(:title, :text)
+    end
 end
