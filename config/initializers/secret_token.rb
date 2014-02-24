0c93edbc762a28ca394c5eec3c4998ae121ebc20
rexno7 <rexno7@gmail.com>
2014-02-23 23:11:54 -0500

    Commit using RubyMine and adding all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'cc35aaed1b56019ebc8fb5a700797b06b648f28fa7bbc59ec26c078f0cec0f429afa14c6040309f11c6121c6e51a967f67d1de2f65ef69590c2579cb7e78c5e1'
